3312df5cc99ebedfb8da1f90e750ad03fbe701b8
sevillecarlos <sevillecarlos@gmail.com>
2021-07-18 23:47:44 -0600

    add jwt, need to setup jwt correctly

--- app/controllers/api/v1/session_controller.rb
+++ app/controllers/api/v1/session_controller.rb
@@ -1,9 +1,15 @@
+require 'jwt'
+
+
 class Api::V1::SessionController < ApplicationController
     def create
-        @user = User.find_by(email: params[:email])
+        hmac_secret = '$C21$'
+        payload = { data: 'test' }
+        token = JWT.encode payload, hmac_secret, 'HS256'
+        @user = User.find_by(email:params["email"])
         if @user
           if @user.password === params[:password]
-            render json: @user, status: 200
+            render json: {user: @user, token: token}, status: 200
           else
             render json: { error: 'Incorrect password' }, status: 400
           end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,4 @@ class User < ApplicationRecord
     validates :first_name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true
-
 end
